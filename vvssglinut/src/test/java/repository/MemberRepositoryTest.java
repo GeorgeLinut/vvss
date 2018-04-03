4f8cedd641235692d2df9ab2b3c3adead86f1b54
George Linut <George.Linut@catalysts.cc>
2018-04-03 13:23:51 +0300

    test1

---
+++ vvssglinut/src/test/java/repository/MemberRepositoryTest.java
@@ -0,0 +1,17 @@
+package repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemberRepositoryTest {
+    @Test
+    public void addMember() throws Exception {
+        assert true;
+    }
+
+    @Test
+    public void checkIfExists() throws Exception {
+    }
+
+}
